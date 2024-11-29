4822018b8e80654e5d47daf6d8593463c5736b20
Justinsmart20 <yanfilbert@gmail.com>
2024-11-29 13:52:08 +0100

    mm

---
+++ src/main/java/com/cmp325/cmp323/Cmp323Application.java
@@ -0,0 +1,13 @@
+package com.cmp325.cmp323;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmp323Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmp323Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmp325/cmp323/Cmp323ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmp325.cmp323;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmp323ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
